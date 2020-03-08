6ca672937c2dc01057e458bc50e8e12ac4685d4e
siddhartha <siddhartha-crypto@protonmail.com>
2020-03-07 19:53:52 -0900

    removed submodules dir, in place of doxy/submodules

---
+++ doxy/my_src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <map>
+
+#include "../include/nlohmann/json.hpp"
+#include "../include/Doxybook/ExceptionUtils.hpp"
